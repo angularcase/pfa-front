305781a9113e3eaa00ed8f503ce6cd20b5214341
tomasz.dobosz <tomasz.dobosz@tiskel.com>
2025-04-09 18:31:43 +0200

    dodanie pages home i contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,6 @@ export class AppComponent {
 
   constructor(private translate: TranslateService) {
     this.translate.setDefaultLang('en');
-    this.translate.use('en');
+    this.translate.use('pl');
   }
 }
