4dcd23c6ff3832eb7ec309d9b5d325739abb3573
tomasz.dobosz <tomasz.dobosz@tiskel.com>
2025-04-10 13:42:02 +0200

    ogarniete w miare generowanie i zapytanie

---
+++ codegen.ts
@@ -0,0 +1,12 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+ 
+const config: CodegenConfig = {
+  schema: 'http://localhost:1337/graphql',
+  documents: './src/app/graphql/queries/*.graphql',
+  generates: {
+    './src/app/graphql/generated.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-apollo-angular']
+    }
+  }
+}
+export default config
--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Apollo, gql } from 'apollo-angular';
 import { ArticleListItemComponent } from "./article-list-item/article-list-item.component";
+import { Article, GetArticlesGQL, GetArticlesQuery } from '../../graphql/generated';
+import { ApolloQueryResult } from '@apollo/client/core';
 
 @Component({
   selector: 'app-articles',
@@ -12,34 +13,44 @@ import { ArticleListItemComponent } from "./article-list-item/article-list-item.
 })
 export class ArticlesComponent {
 
-  articles: any[] = [];
+  articles: Article[] = [];
+
   constructor(
-    private apollo: Apollo,
-    private translate: TranslateService) {
+    // private apollo: Apollo,
+    private getArticlesGQL: GetArticlesGQL,
+    private translate: TranslateService,
+  ) {
   }
 
   ngOnInit(): void {
     const lang = this.translate.currentLang;
 
-    this.apollo
-      .watchQuery({
-        query: gql`
-          query Articles($locale: I18NLocaleCode) {
-            articles(locale: $locale) {
-              body
-              slug
-              title
-            }
-          }
-        `,
-        variables: {
-          locale: lang
-        },
-        fetchPolicy: 'no-cache'
-      })
-      .valueChanges
-      .subscribe((result: any) => {
-        this.articles = result.data.articles;
-      });
+    this.getArticlesGQL.fetch({ locale: lang }).subscribe((result: ApolloQueryResult<GetArticlesQuery>) => {
+      this.articles = result.data.articles.filter((article) => article !== null) as Article[];
+      // this.articles.forEach((article) => {
+      //   console.log(article?.title);
+      // });
+    });
+
+    // this.apollo
+    //   .watchQuery({
+    //     query: gql`
+    //       query Articles($locale: I18NLocaleCode) {
+    //         articles(locale: $locale) {
+    //           body
+    //           slug
+    //           title
+    //         }
+    //       }
+    //     `,
+    //     variables: {
+    //       locale: lang
+    //     },
+    //     fetchPolicy: 'no-cache'
+    //   })
+    //   .valueChanges
+    //   .subscribe((result: any) => {
+    //     this.articles = result.data.articles;
+    //   });
   }
 }
