08f5b53041c7634d4e7681f484bf071b2e072ad5
tomasz.dobosz <tomasz.dobosz@tiskel.com>
2025-04-10 10:19:16 +0200

    laczenie z apollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet, TranslateModule, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  date: Date = new Date();
   
 
-  constructor(private translate: TranslateService) {
-    // this.translate.setDefaultLang('en');
-    // this.translate.use('en');
+  constructor(
+    private apollo: Apollo,
+    private translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.translate.currentLang);
+
+    this.apollo
+      .watchQuery({
+        query: gql`
+          query Articles($locale: I18NLocaleCode) {
+            articles(locale: $locale) {
+              body
+              slug
+              title
+            }
+          }
+        `,
+        variables: {
+          locale: 'pl'
+        }
+      })
+      .valueChanges
+      .subscribe((result: any) => {
+        console.log(result);
+      });
   }
 }
